864a15435a43c09f4d505dff8af28d3a9c8c8085
wwwhickup <toyjena2019@gmail.com>
2023-03-06 07:32:44 -0600

    app and auth layout

--- frontend/src/components/Layout/AuthLayout.tsx
+++ frontend/src/components/Layout/AuthLayout.tsx
@@ -2,9 +2,10 @@ import { Outlet } from 'react-router-dom'
 
 export default function AuthLayout() {
   return (
-    <div>
-      <h1>AuthLayout</h1>
-      <Outlet />
+    <div className='auth-container'>
+      <main className=''>
+        <Outlet />
+      </main>
     </div>
   )
 }
