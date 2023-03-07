75aa5b5ca7a1e82d65fb0fc9e42417b7e6f77c15
wwwhickup <toyjena2019@gmail.com>
2023-03-07 06:32:33 -0600

    dashboard init

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   darkMode: 'class',
   theme: {
@@ -11,9 +12,15 @@ module.exports = {
         'bg-primary': {
           DEFAULT: '#ffffff',
           dark: '#0e172a'  
+        },
+        'bg-secondary': {
+          DEFAULT: '#eeeeee',
+          dark: '#1e293b'
         }
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ]
 }
